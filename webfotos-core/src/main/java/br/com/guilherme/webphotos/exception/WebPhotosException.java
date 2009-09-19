3812639d0bba21ebe0c2b5bbcc2255ea2f594330
rrantz <rrantz@gmail.com>
2009-09-19 02:28:32 +0000

    Clean up Class Rename

---
+++ webfotos-core/src/main/java/br/com/guilherme/webphotos/exception/WebPhotosException.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package br.com.guilherme.webphotos.exception;
+
+/**
+ * @author Guilhe
+ *
+ */
+public class WebPhotosException extends Exception {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = -7335262844624169842L;
+
+    /**
+     *
+     */
+    public WebPhotosException() {
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param arg0
+     */
+    public WebPhotosException(String arg0) {
+        super(arg0);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param arg0
+     */
+    public WebPhotosException(Throwable arg0) {
+        super(arg0);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param arg0
+     * @param arg1
+     */
+    public WebPhotosException(String arg0, Throwable arg1) {
+        super(arg0, arg1);
+        // TODO Auto-generated constructor stub
+    }
+}
