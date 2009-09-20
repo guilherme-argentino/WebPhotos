bf1e139effabf22ff9a5ce7ef9d1e9b9789bf4e6
rrantz <rrantz@gmail.com>
2009-09-20 02:09:33 +0000

    Clean up changed configuration file names to a constant

--- webfotos-core/src/main/java/br/com/guilherme/webphotos/locator/EJBDAOLocator.java
+++ webfotos-core/src/main/java/br/com/guilherme/webphotos/locator/EJBDAOLocator.java
@@ -14,10 +14,10 @@ public class EJBDAOLocator extends BasicEJBLocator {
 		return instance;
 	}
 
-	private static final String REMOTE_TURMASDAO = "java:comp/env/ejb/remote/TurmasDAO";
+	/*private static final String REMOTE_TURMASDAO = "java:comp/env/ejb/remote/TurmasDAO";
 	private static final String REMOTE_MATRICULASDAO = "java:comp/env/ejb/remote/MatriculasDAO";
 	private static final String REMOTE_MEMBERSHIPDAO = "java:comp/env/ejb/remote/MembershipDAO";
-	private static final String REMOTE_CURSOSDAO = "java:comp/env/ejb/remote/CursosDAO";
+	private static final String REMOTE_CURSOSDAO = "java:comp/env/ejb/remote/CursosDAO";*/
 
 	private EJBDAOLocator() {
 		initEJBContext();
