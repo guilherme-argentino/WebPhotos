f8ea4f757c9210861bed3ab7e520508f1651dd68
rrantz <rrantz@gmail.com>
2011-06-02 13:57:05 +0000

    Site settings Class adjustments to JVM v1.6

--- webfotos-core/src/main/java/webfotos/gui/util/TableSorter.java
+++ webfotos-core/src/main/java/webfotos/gui/util/TableSorter.java
@@ -62,7 +62,7 @@ import javax.swing.table.*;
  * @version 2.0 02/27/04
  */
 
-public class TableSorter extends AbstractTableModel {
+public final class TableSorter extends AbstractTableModel {
 	
 	private static final long serialVersionUID = -5572044930561418466L;
 
@@ -154,7 +154,7 @@ public class TableSorter extends AbstractTableModel {
     }
 
     public boolean isSorting() {
-        return sortingColumns.size() != 0;
+        return !sortingColumns.isEmpty();
     }
 
     private Directive getDirective(int column) {
@@ -264,14 +264,17 @@ public class TableSorter extends AbstractTableModel {
         return (tableModel == null) ? 0 : tableModel.getColumnCount();
     }
 
+    @Override
     public String getColumnName(int column) {
         return tableModel.getColumnName(column);
     }
 
+    @Override
     public Class<?> getColumnClass(int column) {
         return tableModel.getColumnClass(column);
     }
 
+    @Override
     public boolean isCellEditable(int row, int column) {
         return tableModel.isCellEditable(modelIndex(row), column);
     }
@@ -280,6 +283,7 @@ public class TableSorter extends AbstractTableModel {
         return tableModel.getValueAt(modelIndex(row), column);
     }
 
+    @Override
     public void setValueAt(Object aValue, int row, int column) {
         tableModel.setValueAt(aValue, modelIndex(row), column);
     }
@@ -379,6 +383,7 @@ public class TableSorter extends AbstractTableModel {
     }
 
     private class MouseHandler extends MouseAdapter {
+        @Override
         public void mouseClicked(MouseEvent e) {
             JTableHeader h = (JTableHeader) e.getSource();
             TableColumnModel columnModel = h.getColumnModel();
