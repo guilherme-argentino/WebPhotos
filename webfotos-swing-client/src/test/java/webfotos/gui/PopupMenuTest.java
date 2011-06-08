ed2a9fa10d5ac6576c7e8b86d753b5d2fb2dd700
rrantz <rrantz@gmail.com>
2011-06-08 16:11:51 +0000

    License adjustments (maven-license-plugin) IzPack bugfixes

--- webfotos-swing-client/src/test/java/webfotos/gui/PopupMenuTest.java
+++ webfotos-swing-client/src/test/java/webfotos/gui/PopupMenuTest.java
@@ -1,10 +1,18 @@
-/*
- * PopupMenuTest.java
- * JUnit based test
+/**
+ * Copyright 2008 WebPhotos
  *
- * Created on 26 de Maio de 2006, 19:03
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 package webfotos.gui;
 
 import junit.framework.TestCase;
