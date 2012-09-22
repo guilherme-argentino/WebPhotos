79f3b62f207cdf1003f080e342e68ab0cd8ab716
rrantz <rrantz@gmail.com>
2012-09-22 00:52:37 +0000

    New Constants

--- netbeans-gui-client/project-type/src/main/java/net/sf/webphotos/netbeans/project/Constants.java
+++ netbeans-gui-client/project-type/src/main/java/net/sf/webphotos/netbeans/project/Constants.java
@@ -1,17 +1,17 @@
 /**
  * Copyright 2008 WebPhotos
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 /*
  * To change this template, choose Tools | Templates
@@ -26,5 +26,11 @@ package net.sf.webphotos.netbeans.project;
 public interface Constants {
 
     public static final String PROJECT_ICON = "net/sf/webphotos/netbeans/project/projectIcon.png";
-    
+
+    public interface ProjectFactory {
+
+        public static final String CONFIG_WEBPHOTOS_PROPERTIES = "config/webphotos.properties";
+        public static final String ALBUNS_WEBPHOTOS_XML = "albuns/webphotos.xml";
+ 
+    }
 }
