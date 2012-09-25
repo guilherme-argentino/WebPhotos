90b4fdacf4f0aa124ff9830e07d7c21db7570e05
rrantz <rrantz@gmail.com>
2012-09-25 19:34:11 +0000

    Organize Imports

--- swing-client/src/main/java/net/sf/webphotos/gui/Visualizador.java
+++ swing-client/src/main/java/net/sf/webphotos/gui/Visualizador.java
@@ -23,7 +23,6 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
