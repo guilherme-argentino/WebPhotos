64297b259b20596c9aab3a853221db0a500a9524
rrantz <rrantz@125a32d9-ff89-4382-b7e0-6d74dfe25e8d>
2012-10-05 03:02:11 +0000

    Functional Tests Adaptation for Cloudbees

--- trunk/swing-client/src/main/java/net/sf/webphotos/WebPhotos.java
+++ trunk/swing-client/src/main/java/net/sf/webphotos/WebPhotos.java
@@ -1,17 +1,17 @@
 /**
  * Copyright 2008 WebPhotos
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package net.sf.webphotos;
 
@@ -24,20 +24,19 @@ import org.apache.derby.drda.NetworkServerControl;
 import org.apache.log4j.Logger;
 
 /**
- * Classe responsável pelo inicio do sistema WebPhotos.
- * Realiza algumas rotinas para início do sistema.
- * Os dados são trabalhados somente no método principal.
+ * Classe responsável pelo inicio do sistema WebPhotos. Realiza algumas rotinas
+ * para início do sistema. Os dados são trabalhados somente no método principal.
  */
 public class WebPhotos {
 
     private static Logger log = Logger.getLogger(WebPhotos.class);
 
     /**
-     * Contêm uma série de rotinas para inicialização do programa.
-     * No primeiro momento, o cache do FTP é apagado.
-     * Logo após é feita uma busca do arquivo de configuração.
-     * é obtido o driver do DB, e são preparadas as conexões para usar Socks Proxy.
-     * Por último é exibida a tela de login ao usuário.
+     * Contêm uma série de rotinas para inicialização do programa. No primeiro
+     * momento, o cache do FTP é apagado. Logo após é feita uma busca do arquivo
+     * de configuração. é obtido o driver do DB, e são preparadas as conexões
+     * para usar Socks Proxy. Por último é exibida a tela de login ao usuário.
+     *
      * @param args Argumentos do método main.
      */
     public static void main(String args[]) {
@@ -51,13 +50,18 @@ public class WebPhotos {
             }
 
             Util.loadSocksProxy();
+            final String derbyDrdaHost = Util.getConfig().getString("derby.drda.host");
+            final int derbyDrdaPortNumber = Util.getConfig().getInt("derby.drda.portNumber");
+            final boolean derbyDrdaStartNetworkServer = Util.getConfig().getBoolean("derby.drda.startNetworkServer");
 
             /**
              * FIXEME: change this code to derby.drda.startNetworkServer=true
              */
-            NetworkServerControl server = new NetworkServerControl(InetAddress.getByName(Util.getConfig().getString("derby.drda.host")), Util.getConfig().getInt("derby.drda.portNumber"));
-            server.start(new PrintWriter(Util.out));
-            server.setMaxThreads(100);
+            if (derbyDrdaStartNetworkServer) {
+                NetworkServerControl server = new NetworkServerControl(InetAddress.getByName(derbyDrdaHost), derbyDrdaPortNumber);
+                server.start(new PrintWriter(Util.out));
+                server.setMaxThreads(100);
+            }
 
             BancoImagem.loadUIManager();
             BancoImagem.loadDBDriver();
@@ -67,7 +71,6 @@ public class WebPhotos {
 
             // Eficiente para produção - Ruim para depuração
             java.awt.EventQueue.invokeLater(new Runnable() {
-
                 @Override
                 public void run() {
                     FrameWebFotos.getInstance().setVisible(true);
