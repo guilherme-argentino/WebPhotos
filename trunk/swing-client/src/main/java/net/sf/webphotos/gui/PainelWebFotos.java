dab2eebc310b62221b479c89ae065277414a4329
rrantz <rrantz@125a32d9-ff89-4382-b7e0-6d74dfe25e8d>
2012-10-05 05:16:56 +0000

    Functional Tests Adaptation for Cloudbees Licenses

--- trunk/swing-client/src/main/java/net/sf/webphotos/gui/PainelWebFotos.java
+++ trunk/swing-client/src/main/java/net/sf/webphotos/gui/PainelWebFotos.java
@@ -1,17 +1,17 @@
 /**
  * Copyright 2008 WebPhotos
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 /*
  * PainelWebFotos.java
@@ -1371,7 +1371,7 @@ public class PainelWebFotos extends javax.swing.JPanel {
         try {
             lstCreditos.setSelectedIndex(0);
         } catch (Exception e) {
-            Util.log("ERRO: " + e.getMessage());
+            Util.log("[PainelWebPhotos.resetFoto()]/ERRO: " + e.getMessage());
         }
         painelFoto.clear();
         painelFoto.getLblFotoInfo().setText("");
@@ -1390,14 +1390,14 @@ public class PainelWebFotos extends javax.swing.JPanel {
         try {
             lstCategoriasAlbum.setSelectedIndex(0);
         } catch (Exception e) {
-            Util.log("ERRO: " + e.getMessage());
+            Util.log("[PainelWebPhotos.resetAlbum()]/ERRO: " + e.getMessage());
         }
         txtDescricao.setText("");
         txtLegenda.setText("");
         try {
             lstCreditos.setSelectedIndex(0);
         } catch (Exception e) {
-            Util.log("ERRO: " + e.getMessage());
+            Util.log("[PainelWebPhotos.resetAlbum()]/ERRO: " + e.getMessage());
         }
 
         TableModelFoto.getModel().update();
