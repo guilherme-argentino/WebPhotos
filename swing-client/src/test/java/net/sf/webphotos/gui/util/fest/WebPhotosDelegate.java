fc60e2ee68462ca89edff77c97ba171ce1436d99
rrantz <rrantz@gmail.com>
2012-10-12 01:56:55 +0000

    Changes to GUI Testing Framework, part two: New Framework Implementation

--- swing-client/src/test/java/net/sf/webphotos/gui/util/fest/WebPhotosDelegate.java
+++ swing-client/src/test/java/net/sf/webphotos/gui/util/fest/WebPhotosDelegate.java
@@ -17,22 +17,26 @@ package net.sf.webphotos.gui.util.fest;
 
 import java.awt.Dialog;
 import java.awt.Frame;
+import static java.awt.event.KeyEvent.*;
 import java.io.File;
 import java.util.Arrays;
+import java.util.Comparator;
 import javax.swing.JButton;
 import net.sf.webphotos.WebPhotos;
+import static org.fest.assertions.Assertions.assertThat;
 import org.fest.swing.core.BasicRobot;
 import org.fest.swing.core.ComponentLookupScope;
 import org.fest.swing.core.GenericTypeMatcher;
 import org.fest.swing.core.MouseButton;
 import org.fest.swing.core.Robot;
+import org.fest.swing.finder.JFileChooserFinder;
 import static org.fest.swing.finder.WindowFinder.*;
 import org.fest.swing.fixture.DialogFixture;
 import org.fest.swing.fixture.FrameFixture;
+import org.fest.swing.fixture.JButtonFixture;
+import org.fest.swing.fixture.JFileChooserFixture;
 import static org.fest.swing.launcher.ApplicationLauncher.*;
-
-import static org.fest.assertions.Assertions.assertThat;
-import org.fest.assertions.Condition;
+import org.fest.swing.security.NoExitSecurityManagerInstaller;
 
 /**
  *
@@ -42,7 +46,20 @@ public class WebPhotosDelegate implements net.sf.webphotos.gui.util.WebPhotosDel
 
     private static final WebPhotosDelegate WEB_PHOTOS_DELEGATE = new WebPhotosDelegate();
     private static final Robot robot;
-    private static FrameFixture mainFrame;
+    private static final int TIMEOUT_SECONDS = 600;
+    
+
+    private static final Comparator<String> IGNORE_CASE_COMPARATOR = new Comparator<String>() {
+
+        @Override
+        public int compare(String o1, String o2) {
+            System.out.println(o1+".compareToIgnoreCase("+o2+") = " + o1.compareToIgnoreCase(o2));
+            return o1.compareToIgnoreCase(o2);
+        }
+    };
+    
+    private FrameFixture mainFrame;
+    private NoExitSecurityManagerInstaller noExitSecurityManagerInstaller;
 
     static {
         robot = BasicRobot.robotWithCurrentAwtHierarchy();
@@ -52,11 +69,8 @@ public class WebPhotosDelegate implements net.sf.webphotos.gui.util.WebPhotosDel
 
     @Override
     public void setUpClass() throws Exception {
-
-//        FailOnThreadViolationRepaintManager.install();
-
-//        ComponentFinder finder = BasicComponentFinder.finderWithCurrentAwtHierarchy();
-//        JOptionPaneFixture optionPane = JOptionPaneFinder.findOptionPane().withTimeout(10000).using(robot);
+        
+        noExitSecurityManagerInstaller = NoExitSecurityManagerInstaller.installNoExitSecurityManager();
 
         java.awt.EventQueue.invokeLater(new Runnable() {
             @Override
@@ -65,25 +79,32 @@ public class WebPhotosDelegate implements net.sf.webphotos.gui.util.WebPhotosDel
             }
         });
 
-        DialogFixture dialog = findDialog(new LoginDialogTypeMatcherImpl()).withTimeout(10, java.util.concurrent.TimeUnit.SECONDS).using(robot);
+        DialogFixture dialog = findDialog(new LoginDialogTypeMatcherImpl("Web")).withTimeout(TIMEOUT_SECONDS, java.util.concurrent.TimeUnit.SECONDS).using(robot);
 
-        dialog.button(new GenericTypeMatcher<JButton>(JButton.class){
-
+        dialog.button(new GenericTypeMatcher<JButton>(JButton.class) {
             @Override
             protected boolean isMatching(JButton okButton) {
                 return "OK".equalsIgnoreCase(okButton.getText()) && okButton.isShowing();
             }
-        
         }).click(MouseButton.LEFT_BUTTON);
 
-        mainFrame =
-                findFrame(new MainFrameTypeMatcherImpl()).withTimeout(20, java.util.concurrent.TimeUnit.SECONDS).using(robot);
-        
+        mainFrame = findFrame(new MainFrameTypeMatcherImpl("Web")).withTimeout(TIMEOUT_SECONDS, java.util.concurrent.TimeUnit.SECONDS).using(robot);
+
     }
 
     @Override
     public void addPhotosToAlbumAndCheck(String[] fileNames, String buttonName, File folderNameToVerify, String fileDialogName) throws RuntimeException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        JButtonFixture button = clickButton(buttonName);
+        JFileChooserFixture fileChooser = JFileChooserFinder.findFileChooser().withTimeout(TIMEOUT_SECONDS, java.util.concurrent.TimeUnit.SECONDS).using(button.robot);
+
+        fileChooser.component().getCurrentDirectory().equals(folderNameToVerify);
+        
+        File[] files = new File[fileNames.length];
+        for (int i = 0; i < files.length; i++) {
+            files[i] = new File(fileNames[i]);
+        }
+        fileChooser.selectFiles(files).pressKey(VK_ENTER);
+        
     }
 
     @Override
@@ -93,7 +114,9 @@ public class WebPhotosDelegate implements net.sf.webphotos.gui.util.WebPhotosDel
 
     @Override
     public void checkComboBoxHasText(String comboBoxName, String textShown) throws RuntimeException {
-        assertThat(Arrays.binarySearch(mainFrame.comboBox(comboBoxName).contents(), textShown) > 0).isTrue();
+        System.out.println("mainFrame.comboBox("+comboBoxName+").contents()" + mainFrame.comboBox(comboBoxName).contents().toString());
+        Arrays.sort(mainFrame.comboBox(comboBoxName).contents(), IGNORE_CASE_COMPARATOR);
+        assertThat(Arrays.binarySearch(mainFrame.comboBox(comboBoxName).contents(), textShown, IGNORE_CASE_COMPARATOR) >= 0).isTrue();
     }
 
     @Override
@@ -103,13 +126,19 @@ public class WebPhotosDelegate implements net.sf.webphotos.gui.util.WebPhotosDel
     }
 
     @Override
-    public void fillModalWithText(String buttonName, String modalTittle, String data) throws RuntimeException {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void fillModalWithText(final String buttonName, String modalTittle, String data) throws RuntimeException {
+        JButtonFixture button = clickButton(buttonName);
+
+        DialogFixture dialog = findDialog(new LoginDialogTypeMatcherImpl(modalTittle)).withTimeout(TIMEOUT_SECONDS, java.util.concurrent.TimeUnit.SECONDS).using(button.robot);
+
+        dialog.textBox().setText(data);
+        dialog.pressKey(VK_ENTER);
     }
 
     @Override
     public void tearDownClass() throws Exception {
         mainFrame.close();
+        noExitSecurityManagerInstaller.uninstall();
     }
 
     @Override
@@ -123,7 +152,6 @@ public class WebPhotosDelegate implements net.sf.webphotos.gui.util.WebPhotosDel
      */
     @Override
     public void setUp() {
-        
     }
 
     /**
@@ -135,27 +163,53 @@ public class WebPhotosDelegate implements net.sf.webphotos.gui.util.WebPhotosDel
         return WEB_PHOTOS_DELEGATE;
     }
 
+    private JButtonFixture clickButton(final String buttonName) {
+        JButtonFixture button = mainFrame.button(new JButtonTypeMatcherImpl(JButton.class, buttonName)).click(MouseButton.LEFT_BUTTON);
+        return button;
+    }
+
     private static class LoginDialogTypeMatcherImpl extends GenericTypeMatcher<Dialog> {
 
-        public LoginDialogTypeMatcherImpl() {
+        private String dialogName;
+
+        public LoginDialogTypeMatcherImpl(final String dialogName) {
             super(Dialog.class);
+            this.dialogName = dialogName;
         }
 
         @Override
         protected boolean isMatching(Dialog frame) {
-            return frame.getTitle().contains("Web") && frame.isShowing();
+            return frame.getTitle().contains(dialogName) && frame.isShowing();
         }
     }
 
     private static class MainFrameTypeMatcherImpl extends GenericTypeMatcher<Frame> {
 
-        public MainFrameTypeMatcherImpl() {
+        private String frameName;
+
+        public MainFrameTypeMatcherImpl(final String frameName) {
             super(Frame.class);
+            this.frameName = frameName;
         }
 
         @Override
         protected boolean isMatching(Frame frame) {
-            return frame.getTitle().contains("Web") && frame.isShowing();
+            return frame.getTitle().contains(frameName) && frame.isShowing();
+        }
+    }
+
+    private static class JButtonTypeMatcherImpl extends GenericTypeMatcher<JButton> {
+
+        private final String buttonName;
+
+        public JButtonTypeMatcherImpl(Class<JButton> supportedType, String buttonName) {
+            super(supportedType);
+            this.buttonName = buttonName;
+        }
+
+        @Override
+        protected boolean isMatching(JButton chosenButtonName) {
+            return buttonName.equalsIgnoreCase(chosenButtonName.getName());
         }
     }
 }
